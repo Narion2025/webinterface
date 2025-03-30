33b1b7f9c6e397588e15b20a88b07013c75afa99
Narion2025 <Ben.Poersch@diyrigent.de>
2025-03-30 03:47:48 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 let lastTrigger = null;
 
